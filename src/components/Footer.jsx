95f68148b122809b49e0b4e0f484d0357f759083
Fede-Giithub <federico.pierdominici@gmail.com>
2025-08-14 17:21:55 -0300

    add: cambie los titulos de las cards

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "lightcoral" }}>
+    <footer style={{ backgroundColor:"lightgreen" }}>
       <p >Sitio desarrollado por <a href="https://www.linkedin.com/in/gabriel-alberini/" target="_blank">Gabriel Alberini</a></p>
     </footer>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -158,17 +158,17 @@ const Home = () => {
   <div className="row">
     {products.map((product) => (
       <div className="col-12 col-md-4 mb-4" key={product.id}>
-        <div className="card h-100 text-center custom-card">
+        <div className="card h-100 text-center custom-card ">
           <img
             className="card-img-top mx-auto d-block"
             style={{ width: "80px", height: "auto" }}
             src={product.image}
             alt={`Imagen de ${product.title}`}
           />
-          <h2 className="card-title">{product.title}</h2>
+          <h2 className="card-title fs-4 fw-bold ">{product.title}</h2>
           <p className="card-text">${product.price}</p>
           <p className="card-text">{product.description}</p>
-          <p className="card-text"><strong>{product.category}</strong></p>
+          <p className="card-text "><strong>{product.category}</strong></p>
           {user && (
             <div>
               <button onClick={() => handleOpenEdit(product)}>Actualizar</button>
