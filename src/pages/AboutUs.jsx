1eb216e82dbb030058a45c316ddb7376dd4f65ed
Fede-Giithub <federico.pierdominici@gmail.com>
2025-08-24 15:29:36 -0300

    add:agrego la página sobre nosotros con su estructura inicial

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import { Layout } from "../components/Layout"
+
+const AboutUs = () => {
+  return (
+    <Layout>
+      <section></section>
+      <section></section>
+      <section></section>
+    </Layout>
+  )
+}
+
+export { AboutUs }
