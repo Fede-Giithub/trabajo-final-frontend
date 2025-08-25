23d082be4478549fa8a2f7263939e6a3cb8941d0
Fede-Giithub <federico.pierdominici@gmail.com>
2025-08-25 18:21:50 -0300

    add:agregué mas estilos a el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor:"lightgreen" }}>
-      <p >Sitio desarrollado por <a href="https://www.linkedin.com/in/gabriel-alberini/" target="_blank">Gabriel Alberini</a></p>
+    <footer style={{ backgroundColor:"lightgreen", position: "fixed",
+      bottom: 0, width: "100%"}}>
+      <p >Sitio desarrollado por <a  style={{ color:"green"}} href="https://www.linkedin.com/in/gabriel-alberini/" target="_blank">Gabriel Alberini</a></p>
     </footer>
   )
 }
